9cb05099f61ebbc64fee41c29662fa6f554eebfc
m1r1k77 <m1r1kzaitsev77@gmail.com>
2023-09-27 11:20:58 +0300

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import Layout from '@/components/layout/Layout';
+import { NextPage } from 'next';
+import Image from 'next/image';
+
+const NotFound: NextPage = () => {
+  return (
+    <Layout>
+      <div style={{ textAlign: 'center', marginTop: '150px' }}>
+        <Image src='/404_error.png' alt='not found' width='700' height='350' />
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFound;
